a502a71718331792c3957454307a43d0eed128d7
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-01-12 23:58:38 +0530

    Profile Image Composable added

--- app/src/main/java/me/svbneelmne/compose/bizzzz/MainActivity.kt
+++ app/src/main/java/me/svbneelmne/compose/bizzzz/MainActivity.kt
@@ -3,15 +3,21 @@ package me.svbneelmne.compose.bizzzz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.svbneelmne.compose.bizzzz.ui.theme.BizzzTheme
@@ -46,7 +52,38 @@ fun CreateBizCard() {
             elevation = 4.dp,
         ) {
 
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ProfileImage()
+                Divider(
+                    thickness = 5.dp
+                )
+            }
+
         }
+    }
+}
+
+@Composable
+private fun ProfileImage() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.shivaprasad),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
